d3009155400cb4a1055186f3e5dffaad8e553622
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-09-01 12:29:54 +0200

    price as simple const variable

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,14 +1,15 @@
 import { computed, signal } from "@preact/signals-react";
 
-const price = signal<number>(12000);
+//const price = signal<number>(12000);
+const price = 3400
 const quantity = signal<number>(100);
-const totalPrice = computed<number>(() => price.value * quantity.value);
+const totalPrice = computed<number>(() => price * quantity.value);
 
 const Products = () => {
   return (
     <div className="p-3">
       <ul className="list-group">
-        <li className="list-group-item">Price : {price.value}</li>
+        <li className="list-group-item">Price : {price}</li>
         <li className="list-group-item">
           Quantity : {quantity.value}
           <button
