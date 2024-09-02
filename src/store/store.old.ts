dba488263ba1c71350f5f8f14080d84ae36ef77d
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-09-02 12:58:50 +0200

    save & load from local storage

--- src/store/store.old.ts
+++ src/store/store.old.ts
@@ -1,31 +1,13 @@
-import { effect, signal } from "@preact/signals-react";
+import { signal } from "@preact/signals-react";
 import { IProductModel } from "../model/product.model";
 
 export class Store {
   // properties
-  products = signal<IProductModel[]>(this.loadFromLocalStorage());
-
-  // load from local storage
-  loadFromLocalStorage() {
-    const data = localStorage.getItem("my-store");
-    if (data == undefined) {
-      return [
-        { id: 1, name: "Computer", price: 4500, selected: false },
-        { id: 2, name: "Printer", price: 2300, selected: true },
-        { id: 3, name: "Smart phone", price: 1200, selected: true },
-      ];
-    } else {
-      return JSON.parse(data);
-    }
-  }
-
-  // constructor
-  constructor() {
-    effect(() => {
-      //console.log("Products updated", this.products.value);
-      localStorage.setItem("my-store", JSON.stringify(this.products.value));
-    });
-  }
+  products = signal<IProductModel[]>([
+    { id: 1, name: "Computer", price: 4500, selected: false },
+    { id: 2, name: "Printer", price: 2300, selected: true },
+    { id: 3, name: "Smart phone", price: 1200, selected: true },
+  ]);
 
   // methods
   // select product
